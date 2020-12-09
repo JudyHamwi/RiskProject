2a025d838e869465410e7bf758bea27d728025a1
dianamiraflor <dianamariemiraflor@cmail.carleton.ca>
2020-12-09 13:57:05 -0500

    AITest, FortifyPhaseTest, PlayerFactoryTest

--- tst/risk/model/player/AITest.java
+++ tst/risk/model/player/AITest.java
@@ -1,13 +1,28 @@
 package risk.model.player;
 
+import org.junit.Before;
 import org.junit.Test;
+import risk.model.board.Country;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class AITest {
+    private static final int PLAYER_ID = 1;
+    Player player;
+    List<Country> countriesOwned;
+
+    @Before
+    public void setUp() {
+        player = new AI(PLAYER_ID, null);
+        countriesOwned = new ArrayList<>();
+    }
 
     @Test
     public void testGetId() {
+        assertEquals(PLAYER_ID, player.getId());
     }
 
     @Test
